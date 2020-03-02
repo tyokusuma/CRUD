6170778feac065ab59080445c2ad42fa1073cbd9
Aristyo Hadikusuma <tyokusuma27@MacBook-Pro-Aristyo.local>
2020-03-02 10:42:43 +0700

    membuat fitur login

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="proses_login.php" align="center" method="post">
+    <h1>Welcome to Administrator</h1><br>
+    Username
+    <input type="text" name="nama" id=""><br>
+    Password
+    <input type="password" name="password" id=""><br>
+    <input type="submit" name="" id="">
+    </form>
+</body>
+</html>
